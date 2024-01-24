762b216ab82f8ff12102e755a98c24765fa38c31
sapols <sapols@yahoo.com>
2024-01-23 19:35:50 -0700

    Only run docker push commands if all docker builds succeed

--- utils/docker_operations.py
+++ utils/docker_operations.py
@@ -8,24 +8,31 @@ from pipeline_utils import *
 def build_and_push_docker_images(docker_folder_path, docker_username, docker_token):
     today = datetime.now().strftime("%Y.%m.%d")
     docker_image_names = get_docker_image_names(docker_folder_path)
+    build_commands = []
+    push_commands = []
 
     for image_name in docker_image_names:
         tag = f"{docker_username}/{image_name}:v{today}"
-        build_command = f"docker build -t {tag} {docker_folder_path}/{image_name}"
-        push_command = f"docker push {tag}"
+        build_commands.append(f"docker build -t {tag} {docker_folder_path}/{image_name}")
+        push_commands.append(f"docker push {tag}")
 
-        try:
-            # Docker login
-            login_command = f"echo {docker_token} | docker login -u {docker_username} --password-stdin"
-            subprocess.run(login_command, shell=True, check=True)
+    try:
+        # Docker login
+        login_command = f"echo {docker_token} | docker login -u {docker_username} --password-stdin"
+        subprocess.run(login_command, shell=True, check=True)
 
-            # Docker build and push
+        # Docker build
+        for build_command in build_commands:
             subprocess.run(build_command, shell=True, check=True)
+
+        # Docker push
+        for push_command in push_commands:
             subprocess.run(push_command, shell=True, check=True)
-            print(f"Successfully built and pushed: {tag}")
-        except subprocess.CalledProcessError as e:
-            print(f"Error during Docker operations for {image_name}: {e}")
-            sys.exit(1)  # Exit the script with an error status
+            print(f"Successfully pushed: `{push_command}`")
+
+    except subprocess.CalledProcessError as e:
+        print(f"Error during Docker operations: {e}")
+        sys.exit(1)  # Exit the script with an error status
 
 if __name__ == '__main__':
     if len(sys.argv) != 4:
