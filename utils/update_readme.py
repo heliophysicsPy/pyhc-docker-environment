9f4220144a902ff16c1c0cb1ae155563dba22089
sapols <sapols@yahoo.com>
2024-01-24 18:06:27 -0700

    Update README table in workflow

--- utils/update_readme.py
+++ utils/update_readme.py
@@ -1,3 +1,8 @@
+"""
+Extracts PyHC package versions from pyhc-environment's requirements.txt and puts them into a table in the README.
+"""
+
+import os
 import re
 
 from generate_dependency_table import *
@@ -18,16 +23,47 @@ def extract_versions_from_requirements(requirements_path, package_names):
         else:
             # Extract version from requirements.txt
             for line in requirements:
-                if package_name.lower() in line.lower():
-                    match = re.search(r'==(.+?)(\s*#|$)', line)
-                    if match:
-                        versions[package_name] = match.group(1).strip()
-                    break
+                # Regex to match the package name at the start of the line or after a comment
+                match = re.match(rf"(^|\#\s*){package_name.lower()}([><=]=?)", line.lower())
+                if match:
+                    version_match = re.search(r'([><=]=?\s*)([^#\s]+)', line)
+                    if version_match:
+                        versions[package_name] = version_match.group(2).strip()
+                        break
     return versions
 
 
 def versions_to_markdown_table(versions):
     table = "Package | Version\n---|---\n"
-    for package, version in versions.items():
-        table += f"{package} | {version}\n"
+    for package in sorted(versions, key=lambda x: x.lower()):
+        table += f"{package} | {versions[package]}\n"
     return table
+
+
+def update_readme_with_table(readme_path, section_header, new_table):
+    """
+    Removes everything below the README's section header and replaces with the new_table
+    """
+    with open(readme_path, 'r') as file:
+        lines = file.readlines()
+
+    # Find the index of the section header
+    section_index = next((i for i, line in enumerate(lines) if section_header in line), None)
+
+    # If section is found, update the README
+    if section_index is not None:
+        updated_readme = lines[:section_index + 1] + [new_table]
+        with open(readme_path, 'w') as file:
+            file.writelines(updated_readme)
+
+
+if __name__ == '__main__':
+    requirements_file_path = os.path.join(os.path.dirname(os.path.dirname(os.path.abspath(__file__))), 'docker', 'pyhc-environment', 'contents', 'requirements.txt')
+    pyhc_packages = get_core_pyhc_packages() + get_other_pyhc_packages()
+
+    package_versions = extract_versions_from_requirements(requirements_file_path, pyhc_packages)
+    md_table = versions_to_markdown_table(package_versions)
+
+    readme_file_path = os.path.join(os.path.dirname(os.path.dirname(os.path.abspath(__file__))), 'README.md')
+    section_header = "## PyHC Package Versions in Current Environment"
+    update_readme_with_table(readme_file_path, section_header, md_table)
