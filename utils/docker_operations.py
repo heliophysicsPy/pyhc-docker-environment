555aa43aa27e7b6c037c222a289747713d8d5d96
sapols <sapols@yahoo.com>
2024-02-13 18:13:02 -0700

    Push and remove Docker images immediately after creation

--- utils/docker_operations.py
+++ utils/docker_operations.py
@@ -8,31 +8,41 @@ from pipeline_utils import *
 def build_and_push_docker_images(docker_folder_path, docker_username, docker_token):
     today = datetime.now().strftime("%Y.%m.%d")
     docker_image_names = get_docker_image_names(docker_folder_path)
-    build_commands = []
-    push_commands = []
-
-    for image_name in docker_image_names:
-        tag = f"{docker_username}/{image_name}:v{today}"
-        build_commands.append(f"docker build -t {tag} {docker_folder_path}/{image_name}")
-        push_commands.append(f"docker push {tag}")
 
     try:
         # Docker login
         login_command = f"echo {docker_token} | docker login -u {docker_username} --password-stdin"
         subprocess.run(login_command, shell=True, check=True)
 
-        # Docker build
-        for build_command in build_commands:
+        for image_name in docker_image_names:
+            tag = f"{docker_username}/{image_name}:v{today}"
+
+            # Build the Docker image
+            build_command = f"docker build -t {tag} {docker_folder_path}/{image_name}"
+            print(f"Building image: {tag}")
             subprocess.run(build_command, shell=True, check=True)
 
-        # Docker push
-        for push_command in push_commands:
+            # Push the Docker image
+            push_command = f"docker push {tag}"
+            print(f"Pushing image: {tag}")
             subprocess.run(push_command, shell=True, check=True)
-            print(f"Successfully pushed: `{push_command}`")
+
+            # Remove the Docker image to free up disk space
+            remove_command = f"docker rmi {tag}"
+            print(f"Removing image: {tag}")
+            subprocess.run(remove_command, shell=True, check=True)
+
+            print(f"Successfully processed: {tag}")
 
     except subprocess.CalledProcessError as e:
-        print(f"Error during Docker operations: {e}")
+        print(f"Error during Docker operations: {e}", flush=True)
+        print("::set-output name=should_run::false", flush=True)
         sys.exit(1)  # Exit the script with an error status
+    except Exception as e:
+        print(f"Unhandled exception: {e}", flush=True)
+        print("::set-output name=should_run::false", flush=True)
+        sys.exit(1)  # Exit the script with an error status
+
 
 if __name__ == '__main__':
     if len(sys.argv) != 4:
