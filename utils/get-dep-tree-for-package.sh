fdaea6bff7348d6468d3821f81d77b9acc19454c
sapols <sapols@yahoo.com>
2025-06-24 14:15:19 -0600

    Avoid dot/underscore clash setuptools bug for all packages

--- utils/get-dep-tree-for-package.sh
+++ utils/get-dep-tree-for-package.sh
@@ -13,7 +13,7 @@ source $TEMP_ENV_NAME/bin/activate
 
 # Install the given package and store its pipdeptree output
 PIP_INSTALL_OUTPUT_0=$(pip install wheel)
-PIP_INSTALL_OUTPUT_1=$(pip install $PACKAGE)
+PIP_INSTALL_OUTPUT_1=$(pip install --use-pep517 $PACKAGE)  # note: added `--use-pep517` June 24, 2025 because some older packages started failing with a dot/underscore clash setuptools bug 
 PIP_INSTALL_OUTPUT_2=$(pip install -q pipdeptree==2.3.3)
 
 # Remove '==<version' from $PACKAGE if given (unnecessary now that we pass $BASE_PACKAGE to pipdeptree)
