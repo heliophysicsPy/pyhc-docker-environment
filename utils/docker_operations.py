ef94c4bacbab1f38613945cb0e9d69f56a64f987
sapols <sapols@yahoo.com>
2025-09-18 16:55:54 -0600

    Revert tags back to {today}

--- utils/docker_operations.py
+++ utils/docker_operations.py
@@ -15,7 +15,7 @@ def build_and_push_docker_images(docker_folder_path, docker_username, docker_tok
         subprocess.run(login_command, shell=True, check=True)
 
         for image_name in docker_image_names:
-            date_tag = f"{docker_username}/{image_name}:v2025.09.16"
+            date_tag = f"{docker_username}/{image_name}:v{today}"
             latest_tag = f"{docker_username}/{image_name}:latest"
 
             # Build the Docker image with the date-based tag
@@ -52,7 +52,7 @@ def build_and_push_docker_images(docker_folder_path, docker_username, docker_tok
         # If we reach this point, we have successfully built and pushed today's images.
         # Set the output variable to the date-based version tag (e.g., v2024.12.19).
         # This assumes all images use the same date tag.
-        print(f"::set-output name=docker_version::v2025.09.16")
+        print(f"::set-output name=docker_version::v{today}")
 
     except subprocess.CalledProcessError as e:
         print(f"Error during Docker operations: {e}", flush=True)
